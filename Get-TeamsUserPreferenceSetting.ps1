27b3bdafcfbca039f9f2bc0735df0c1bc1c22ee8
anothervictim <3654992+anothervictim@users.noreply.github.com>
2022-05-31 09:02:55 -0500

    Add files via upload

---
+++ Get-TeamsUserPreferenceSetting.ps1
@@ -0,0 +1,19 @@
+function Get-TeamsUserPreferenceSetting {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$false)]
+        [String]$jsonfile = $env:AppData + '\Microsoft\Teams\desktop-config.json',
+        [String]$User = $env:USERDOMAIN + '\' + $env:USERNAME
+    )
+    
+    Write-Verbose ($jsonfile)
+    Write-Verbose ($User)
+
+    if (Test-Path $jsonfile) {
+        $json = Get-Content $jsonfile | ConvertFrom-Json
+        $json.appPreferenceSettings | Out-String
+    }
+    else {
+        Write-Error ("file does not exist: {0}", $jsonfile)
+    }
+}
---
+++ Set-TeamsUserPreferenceSetting.ps1
@@ -0,0 +1,54 @@
+function Get-TeamsUserPreferenceSetting {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True)]
+        [ValidateSet(
+            'openAtLogin',
+            'openAsHidden',
+            'runningOnClose',
+            'disableGpu',
+            'registerAsIMProvider',
+            'enableMediaLoggingPreferenceKey'
+            )
+        ]
+        [String]$PreferenceSetting,
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateSet(
+            'True',
+            'False'
+            )
+        ]
+        [String]$PreferenceValue,
+
+        [Parameter(Mandatory=$false)]
+        [String]$jsonfile = $(Join-Path $env:AppData 'Microsoft\Teams\desktop-config.json'),
+        [Parameter(Mandatory=$false)]
+        [String]$User = $(Join-Path $env:USERDOMAIN $env:USERNAME)
+    )
+    
+    Write-Verbose ($jsonfile)
+    Write-Verbose ($User)
+
+    if (Test-Path $jsonfile) {
+        # Terminate running process for user
+        $Process = Get-Process -Name 'Teams' -IncludeUserName -ErrorAction SilentlyContinue |
+            Where-Object UserName -eq $User
+        if ($Process) {
+            $Killed = Stop-Process -Name $($Process.Name) -Force -PassThru
+        }
+
+        $json = Get-Content $jsonfile | ConvertFrom-Json
+        Write-Verbose ($json.appPreferenceSettings | Out-String)
+    
+        $json.appPreferenceSettings.$PreferenceSetting = $PreferenceValue
+        $json | ConvertTo-Json | Out-File $jsonfile -Encoding ASCII
+        Write-Verbose ("File has been modified.")
+        if ($Killed) {
+            & $(Join-Path $env:LOCALAPPDATA 'Microsoft\Teams\Update.exe') --processStart "Teams.exe"
+        }
+    }
+    else {
+        Write-Error ("file does not exist: {0}", $jsonfile)
+    }
+}
